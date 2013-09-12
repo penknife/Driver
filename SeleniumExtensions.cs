1029c4b019698d87aedfeab43b3344261440e3b5
penknife <idokolya@gmail.com>
2013-09-12 11:35:47 +0300

    selenium extensions
    extension for selenium

---
+++ SeleniumExtensions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+
+namespace Driver
+{
+    static class SeleniumExtensions
+    {
+        private static object @by;
+
+        public static void WaitForElement(this IWebDriver driver, By by)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until<bool>((d) =>
+                {
+                    try
+                    {
+                        if (d.FindElement(by).Displayed)
+                            return true;
+                        else
+                            return false;
+                    }
+                    catch (NoSuchElementException e)
+                    {
+                        return false;
+                    }
+                    catch (ElementNotVisibleException)
+                    {
+                        return false;
+                    }
+                    catch (StaleElementReferenceException)
+                    {
+                        return false;
+                    }
+                    
+                });
+        }
+
+        
+            
+        public static bool IsPresent(this IWebDriver driver, By bylocator)
+        {
+
+            bool variable = false;
+            try
+            {
+                IWebElement element = driver.FindElement(bylocator);
+                variable = element != null;
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+            return variable;
+        }
+
+
+        public static void SendKeys(this IWebElement element, string value, bool clearFirst)
+        {
+            if (clearFirst) element.Clear();
+            element.SendKeys(value);
+        }
+
+        public static string GetText(this IWebDriver driver, By by)
+        {
+            return driver.FindElement(By.TagName("body")).Text;
+        }
+
+        public static bool HasElement(this IWebDriver driver, By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static IJavaScriptExecutor Scripts(this IWebDriver driver)
+        {
+            return (IJavaScriptExecutor) driver;
+        }
+
+
+        public static bool HasElement(this IWebElement element, By by)
+        {
+            try
+            {
+                element.FindElement(by);
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return false;
+            }
+             
+            return true;
+        }
+
+     
+/*        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+    {
+        if (timeoutInSeconds > 20)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(drv => drv.FindElement(by));
+        }
+        return driver.FindElement(by);
+    }*/
+        public static IWebElement FindElement(this ISearchContext context, By by, uint timeout, bool displayed = false)
+        {
+            var wait = new DefaultWait<ISearchContext>(context);
+            wait.Timeout = TimeSpan.FromSeconds(timeout);
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            return wait.Until(ctx =>
+            {
+                var elem = ctx.FindElement(by);
+                if (displayed && !elem.Displayed)
+                    return null;
+
+                return elem;
+            });
+        }
+
+        public static void WaitForPageToLoad(this IWebDriver driver)
+        {
+            TimeSpan timeout = new TimeSpan(0, 0, 30);
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+
+            IJavaScriptExecutor javascript = driver as IJavaScriptExecutor;
+            if (javascript == null)
+                throw new ArgumentException("driver", "Driver must support javascript execution");
+
+            wait.Until((d) =>
+            {
+                try
+                {
+                    string readyState = javascript.ExecuteScript("if (document.readyState) return document.readyState;").ToString();
+                    return readyState.ToLower() == "complete";
+                }
+                catch (InvalidOperationException e)
+                {
+                    //Window is no longer available
+                    return e.Message.ToLower().Contains("unable to get browser");
+                }
+                catch (WebDriverException e)
+                {
+                    //Browser is no longer available
+                    return e.Message.ToLower().Contains("unable to connect");
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public static void SetAttribute(this IWebElement element, string attributeName, string value)
+        {
+            IWrapsDriver wrappedElement = element as IWrapsDriver;
+            if (wrappedElement == null)
+                throw new ArgumentException("element", "Element must wrap a web driver");
+
+            IWebDriver driver = wrappedElement.WrappedDriver;
+            IJavaScriptExecutor javascript = driver as IJavaScriptExecutor;
+            if (javascript == null)
+                throw new ArgumentException("element", "Element must wrap a web driver that supports javascript execution");
+
+            javascript.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, attributeName, value);
+        }
+
+        public static T GetAttributeAsType<T>(this IWebElement element, string attributeName)
+        {
+            string value = element.GetAttribute(attributeName) ?? string.Empty;
+            return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(value);
+        }
+
+        public static T TextAsType<T>(this IWebElement element)
+        {
+            string value = element.Text;
+            return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(value);
+        }
+    }
+}
