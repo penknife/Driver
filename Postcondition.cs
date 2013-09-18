dc951279a566ddfa818f1af6a34775e574045d98
penknife <idokolya@gmail.com>
2013-09-18 18:36:01 +0300

    SQl query commit
    example webdriver connection to sql

---
+++ Postcondition.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace SensingPortalTests.Helpers
+{
+    public class PostCondition
+    {
+       public static void DeleteDbFolder(PostCondition postCondition)
+        {
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = new SqlConnection("Server=server;Database=somebase;User Id=User;Password=Password;");
+            const string sql = @"some query";
+            comm.CommandText = sql;
+            comm.Connection.Open();
+             comm.ExecuteReader();
+            comm.Connection.Close();
+            }
+    }
+}
