3a945144311c2116b4ffb0d4eaab69b2a85646e2
penknife <idokolya@gmail.com>
2013-09-26 15:47:29 +0300

    Execute Scalar return a value example
    Some example with return a value from SQL query result

--- Postcondition.cs
+++ Postcondition.cs
@@ -14,5 +14,25 @@ namespace SensingPortalTests.Helpers
              comm.ExecuteReader();
             comm.Connection.Close();
             }
+       public static void DeleteDbDocument()
+       {
+           SqlConnection sqlConnection1 = new SqlConnection(SqlServer);
+           SqlCommand comm = new SqlCommand();
+           Object reportIdValue;
+
+           comm.CommandText = "select quary";
+           comm.CommandType = CommandType.Text;
+           comm.Connection = sqlConnection1;
+
+           sqlConnection1.Open();
+
+           reportIdValue = comm.ExecuteScalar();
+
+
+           comm.CommandText = "delete quary" + reportIdValue;
+           comm.ExecuteReader();
+           sqlConnection1.Close();
+
+       }
     }
 }
